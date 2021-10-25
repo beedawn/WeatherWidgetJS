8ceaed0a640b05ae204658b32caafd3d8880f2d6
bee-dmg <brandonschmersal@gmail.com>
2021-10-25 18:11:45 -0400

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import Weather from './components/weather';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import Weather from "./components/weather";
+import { Dimmer, Loader } from "semantic-ui-react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   const [lat, setLat] = useState([]);
@@ -10,34 +12,41 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log(setLat(position.coords.latitude));
-      setLong(position.coords.longitude);
-      
-    });
-    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result)
-      
-      console.log(result);
-    });
-  }
-  fetchData();
-}, [lat,long])
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(setLat(position.coords.latitude));
+        setLong(position.coords.longitude);
+      });
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
 
-  return (<div className="App">
-        {(typeof data.main != 'undefined') ? (
-          <Weather weatherData={data}/>
-        ) : (
-          <div>
-          <Dimmer active>
-            <Loader>Loading..</Loader>
-          </Dimmer>
-       </div>
-        )}
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, long]);
 
-  </div>
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+            <Col>
+          {typeof data.main != "undefined" ? (
+            <Weather weatherData={data} />
+          ) : (
+            <div>
+              <Dimmer active>
+                <Loader>Loading..</Loader>
+              </Dimmer>
+            </div>
+          )}
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
